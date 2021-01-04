bc4ea3a50afbfb5af5b857b66491fb42eab1b3e1
feernando <fernandofitilan@hotmail.com>
2021-01-04 09:23:17 -0300

    up

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: 'O novo prefeito tem condição de atuar como gestor da cidade?',
+    info:
+      'Não, ele não tem nenhuma capacidade mental, cultural e nem acadêmica... Então eu não apoio e para mim a cidade ficará num retocesso!',
+  },
+  {
+    id: 2,
+    title: 'Conpensa uma pessoa morar em Lambari?',
+    info:
+      'Depende de cada caso, vou por 2 exemplos... Se a pessoa tiver uma condição de vida e nçao depender dela, compensa sim, mas se não tiver e for depender da cidade, não compensa de jeito nenhum',
+  },
+  {
+    id: 3,
+    title: 'Esportes como anda em Lambari?',
+    info:
+      ' A sociedade é que se vira para poder ter, por quê se for depender do município nem quadra tem.',
+  },
+  {
+    id: 4,
+    title: 'Lambari passa por nescessidades?',
+    info:
+      'Passa bastante e não tem condição nenhuma de revertar a situação no momento.',
+  },
+  {
+    id: 5,
+    title: 'Quando Lambari terá turistas?',
+    info:
+      'Provavelmente quando a cidade tiver um prefeito capacitado e uma sociedade conciêntizada em não vender votos ',
+  },
+]
+export default questions
